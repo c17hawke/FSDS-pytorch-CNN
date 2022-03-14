eed8bd5a3496161e667840cbb219f15d6d820cc2
c17hawke <sunny.c17hawke@gmail.com>
2022-03-14 13:03:37 +0530

    deom added

--- init_setup.sh
+++ init_setup.sh
@@ -5,4 +5,6 @@ echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
+echo [$(date)]: "install PyTorch"
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
 echo [$(date)]: "END"
